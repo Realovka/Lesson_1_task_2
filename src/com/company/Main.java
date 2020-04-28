95e165fc8575fbfd7c0f998b1b90c699de0e5802
Nastya <realovka@mail.ru>
2020-04-28 10:44:22 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите первую сторону треугольника");
+        Scanner scan1=new Scanner(System.in);
+        int a=scan1.nextInt();
+
+        System.out.println("Введите вторую сторону треугольника");
+        Scanner scan2=new Scanner(System.in);
+        int b=scan2.nextInt();
+
+        System.out.println("Введите третью сторону треугольника");
+        Scanner scan3=new Scanner(System.in);
+        int c=scan3.nextInt();
+
+        if ((a+b>c)&(b+c>a)&(a+c>b)) {
+            System.out.println("Такой треугольник существует");
+        } else {
+            System.out.println("Такой треугольник не существует");
+        }
+
+    }
+}
